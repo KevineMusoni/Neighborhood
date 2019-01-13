7b40e48b7e36aefb7aa7aac18d6236e9b4545c88
Kevine Musoni <kevinenicky@gmail.com>
2019-01-13 22:55:45 +0300

    search view  function added

--- hood/views.py
+++ hood/views.py
@@ -40,3 +40,40 @@ def index(request):
     date = dt.date.today()
     hoods = Neighbourhood.objects.all()
     return render(request, 'index.html',{"date":date, "hoods":hoods})
+
+
+def profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    hoods = Neighbourhood.objects.all()
+    return render(request, 'profile/profile.html', {"date": date, "profile":profile,"hoods":hoods})
+
+def edit_profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    if request.method == 'POST':
+        signup_form = EditForm(request.POST, request.FILES,instance=request.user.profile)
+        if signup_form.is_valid():
+            signup_form.save()
+            return redirect('profile')
+    else:
+        signup_form =EditForm()
+
+    return render(request, 'profile/edit_profile.html', {"date": date, "form":signup_form,"profile":profile})
+
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_businesses = Business.objects.filter(name=search_term)
+        message = f"{search_term}"
+        profiles=  Profile.objects.all( )
+
+        return render(request, 'search.html',{"message":message,"business": searched_businesses,'profiles':profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
