e85ce41777ebb70c7d125f99f70a33ee52fe5729
Kevine Musoni <kevinenicky@gmail.com>
2019-01-13 21:32:47 +0300

    post form created

--- hood/forms.py
+++ hood/forms.py
@@ -3,11 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import *
 
+
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
 
 class NeighoodForm(forms.ModelForm):
     class Meta:
@@ -18,6 +20,13 @@ class EditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio','profile_pic']
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+            model = Business
+            exclude = ['neighbourhood', 'profile']
+
 
 class PostForm(forms.ModelForm):
     class Meta:
