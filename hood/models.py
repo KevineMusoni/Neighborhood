bfd7840a768acf83562c2080e64c36844da5ac79
Kevine Musoni <kevinenicky@gmail.com>
2019-01-13 19:14:18 +0300

    neighbourhood business class created

--- hood/models.py
+++ hood/models.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
-    full_name = models.CharField(max_length=30)
+    full_name = models.CharField(max_length=100)
     bio = models.CharField(max_length=100)
     profile_pic = models.ImageField(upload_to='profile/')
     neighbourhood = models.ForeignKey('Neighbourhood', blank=True, null=True)
@@ -39,12 +39,31 @@ def save_user_profile(sender, instance, **kwargs):
 
 
 class Neighbourhood(models.Model):
-    name = models.CharField(max_length = 300)
+    name = models.CharField(max_length = 100)
     image = models.ImageField(upload_to='neighimage/', null=True)
     admin = models.ForeignKey(Profile, related_name='hoods', null=True)
-    description = models.CharField(max_length = 300,default='this is my hood y all...')
+    description = models.CharField(max_length = 100,default='this is my hood y all...')
     def save_neighbourhood(self):
         self.save()
     def delete_neighbourhood(self):
         self.delete()
 
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10)
+    image = models.ImageField(upload_to='bsimage/')
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name='businesses')
+    description = models.CharField(max_length = 100)
+    profile = models.ForeignKey(Profile, related_name='profiles')
+    def save_business(self):
+        self.save()
+    def delete_business(self):
+        self.delete()
+    @classmethod
+    def search_by_name(cls,search_term):
+        business = cls.objects.filter(title__icontains=search_term)
+        return business
+
+
+    
