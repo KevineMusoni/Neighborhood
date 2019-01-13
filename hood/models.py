fd2fb94a6ca7c149d38e413aad90c6ed61fc5e1b
Kevine Musoni <kevinenicky@gmail.com>
2019-01-13 23:52:54 +0300

    category class added & created requirements file

--- hood/models.py
+++ hood/models.py
@@ -83,3 +83,19 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+class Comment(models.Model):
+  comment = models.TextField()
+  post = models.ForeignKey(Post,on_delete=models.CASCADE)
+  postername= models.ForeignKey(User, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField(auto_now_add=True)
--- hood/views.py
+++ hood/views.py
@@ -7,6 +7,14 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import *
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
 # Create your views here.
 
 # app view functions
