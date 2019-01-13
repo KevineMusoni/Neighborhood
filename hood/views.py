3b0c7a8cf3b7b7b7b2e6c50bf9cec9eb5ffc8e60
Kevine Musoni <kevinenicky@gmail.com>
2019-01-13 23:21:14 +0300

    adding new business functionality added

--- hood/views.py
+++ hood/views.py
@@ -117,3 +117,21 @@ def newcomment(request,id):
         form = NewCommentForm()
 
     return render(request, 'newcomment.html',{'brush':brush,"comments":comments,"form":form})
+
+
+def post_business(request,id):
+    date = dt.date.today()
+    hood=Neighbourhood.objects.get(id=id)
+    business = Business.objects.filter(neighbourhood=hood)
+    form = BusinessForm()
+    if request.method == 'POST':
+        form = BusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.profile = request.user.profile
+            business.neighbourhood = hood
+            business.save()
+            return redirect('index')
+    else:
+        form = BusinessForm()
+        return render(request,'new_business.html',{"form":form,"business":business,"hood":hood,  "date":date})
